9827d8a4aec20e932dd56a704640788831f88d78
Mjkp <kpmoon@gmail.com>
2022-10-04 11:26:48 +0900

    citation added

--- filament_calibrator.py
+++ filament_calibrator.py
@@ -4,6 +4,12 @@ from tkinter import Y
 from numpy import true_divide
 from RsNGA import *
 
+import serial
+import time
+
+ser = serial.Serial('COM11',9600,timeout=1)  # open serial port
+print(ser.name)         # check which port was really used
+
 CH_ONE = 1
 CH_TWO = 2
 MODEL = 'USB0::0x0AAD::0x0197::5601.8002k05-101076::INSTR'
@@ -13,16 +19,47 @@ DERIVATIVE_TOLERANCE = 0.00001
 PLATEAU_DURATION = 3
 LOG = False
 
-POWER_DURATION = 10 # in seconds
+POWER_DURATION = 30 # in seconds
 MAX_VOLTAGE_CH_ONE = 100
-MAX_CURRENT_CH_ONE = 0.001
+MAX_CURRENT_CH_ONE = 0.2
 MAX_VOLTAGE_CH_TWO = 5
 MAX_CURRENT_CH_TWO = 0.005
-INST_MODE = "OFF"
+INST_MODE = "OFF" #instrument mode- "OFF", "SERIES" or "PARALLEL"
 stop_when_plateau = False
 
+DEBOUNCING = 5 # seconds
+
 powersupply = RsNGA(MODEL,logmode=LOG,mode=INST_MODE,debouncing=INSTR_DEBOUNCING)
-powersupply.powerChannels(POWER_DURATION,MAX_VOLTAGE_CH_ONE, MAX_CURRENT_CH_ONE,log_data=True)
+# powersupply.powerChannels(POWER_DURATION,MAX_VOLTAGE_CH_ONE, MAX_CURRENT_CH_ONE,log_data=True)
+powersupply.powerChannels(MAX_VOLTAGE_CH_ONE, MAX_CURRENT_CH_ONE)
+
+calibration = True
+target_resistance = 95 
+res_vals = [400,400,400,400] # arbitraty numbers that is over the target resistance
+relay_counter = 0
+
+time.sleep(DEBOUNCING/2)
+ser.write("{c}\n".format(c = relay_counter).encode('utf-8'))
+print(ser.readline())
+
+time.sleep(DEBOUNCING) # time for power supply 
+
+while calibration:
+    res_vals[relay_counter] = powersupply.getVoltage(CH_ONE)/powersupply.getCurrent(CH_ONE)
+    print(res_vals[relay_counter])
+    if( res_vals[relay_counter] <target_resistance) :
+        print("next pixel")
+        relay_counter +=1
+        if(relay_counter > (len(res_vals)-1)):
+            print("all calibrated")
+            relay_counter = -1
+            calibration = False
+        ser.write("{c}\n".format(c = relay_counter).encode('utf-8'))
+        time.sleep(DEBOUNCING) # time for power supply to recalculate ... 
+
+
+# time.sleep(5)
+powersupply.powerOFF()
 powersupply.closePowerSupply()
 
 '''
